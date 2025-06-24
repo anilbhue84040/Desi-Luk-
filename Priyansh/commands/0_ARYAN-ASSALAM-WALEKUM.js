f04101733bd5fb3d7febbead0d1973b296ab025f
anilbhue84040 <anilbhue840@gmail.com>
2025-06-24 13:06:31 +0530

    Update 0_ARYAN-ASSALAM-WALEKUM.js

--- Priyansh/commands/0_ARYAN-ASSALAM-WALEKUM.js
+++ Priyansh/commands/0_ARYAN-ASSALAM-WALEKUM.js
@@ -3,8 +3,8 @@ module.exports.config = {
         name: "BYE",
     version: "1.1.1",
         hasPermssion: 0,
-        credits: "ARYAN", 
-        description: "THIS BOT IS ARYAN",
+        credits: "Chery x Ritu", 
+        description: "THIS BOT IS Ritu",
         commandCategory: "no prefix",
     cooldowns: 5, 
 };
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Assalam alaikum") ||
 react.includes("assalamualaikum")) {
                 var msg = {
-                                body: `──────────────────\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n──────────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  💐𝐒𝐇𝐀𝐀𝐍💐─`,
+                                body: `──────────────────\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n──────────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  💐Chery x Ritu💐─`,
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
